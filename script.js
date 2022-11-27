2b32f726aecf50e59bc7bfddf5b5e92fa6533112
Shaxzod <tursunmuratoshaxzod@gmail.com>
2022-11-27 17:02:34 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,128 @@
+let arr = [
+    {
+        id: Math.random(),
+        name: "Timur",
+        info: {
+            school: "235",
+            faculity: "SMM",
+        },
+    },
+    {
+        id: Math.random(),
+        name: "Imran",
+        info: {
+            school: "ne izvestno",
+            faculity: "programming",
+        },
+    },
+    {
+        id: Math.random(),
+        name: "Aminjon",
+        info: {
+            school: "444",
+            faculity: "Dizayn",
+        },
+    },
+    {
+        id: Math.random(),
+        name: "Maxmud",
+        info: {
+            school: "777",
+            faculity: "3dsmax",
+        },
+    },
+    {
+        id: Math.random(),
+        name: "Muxammad",
+        info: {
+            school: "5555",
+            faculity: "Backend",
+        },
+    },
+    {
+        id: Math.random(),
+        name: "Timur",
+        info: {
+            school: "235",
+            faculity: "SMM",
+        },
+    },
+    {
+        id: Math.random(),
+        name: "Imran",
+        info: {
+            school: "ne izvestno",
+            faculity: "programming",
+        },
+    },
+    {
+        id: Math.random(),
+        name: "Aminjon",
+        info: {
+            school: "444",
+            faculity: "Dizayn",
+        },
+    },
+    {
+        id: Math.random(),
+        name: "Maxmud",
+        info: {
+            school: "777",
+            faculity: "3dsmax",
+        },
+    },
+    {
+        id: Math.random(),
+        name: "Maxmud",
+        info: {
+            school: "777",
+            faculity: "3dsmax",
+        },
+    },
+    {
+        id: Math.random(),
+        name: "Muxammad",
+        info: {
+            school: "5555",
+            faculity: "Backend",
+        },
+    },
+];
+
+let categories = [
+    {
+        course: "SMM",
+        count: 0,
+    },
+    {
+        course: "PROGRAMMING",
+        count: 0,
+    },
+    {
+        course: "3DSMAX",
+        count: 0,
+    },
+    {
+        course: " DIZAYN",
+        count: 0,
+    },
+    {
+        course: "BACKEND",
+        count: 0,
+    },
+];
+
+for (let itemTwo of categories) {
+    itemTwo.uchenik = []         
+
+}
+
+for (let item of arr) {
+    for(let itemTwo of categories) {
+        if(item.info.faculity.toUpperCase() === itemTwo.course.toUpperCase()) {
+            itemTwo.count++
+            itemTwo.uchenik.push(item)
+        }
+    }
+}
+console.table(categories)
